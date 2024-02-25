100d50dc87cd8ce3b4370057f54cfcd87a38d158
iiriis <avijitdasxp@gmail.com>
2024-02-25 22:06:58 +0530

    updated startup code

--- src/__startup.c
+++ src/__startup.c
@@ -5,9 +5,9 @@ extern uint32_t _MSP;  // Defined in link.ld
 
 void Reset_Handler(void) {
 
-    extern uint8_t __data_start__, __data_end__, __data_FLASH_end__, __bss_start__, __bss_end__;
+    extern uint8_t __data_start__, __data_end__, __data_FLASH_start__, __bss_start__, __bss_end__;
     /* Copy the .data from flash to RAM */
-    memcpy(&__data_start__, &__data_FLASH_end__, (uint32_t)(&__data_end__ - &__data_start__));
+    memcpy(&__data_start__, &__data_FLASH_start__, (uint32_t)(&__data_end__ - &__data_start__));
     /* Fill the .bss section in RAM with all Zeros */
     memset(&__bss_start__, 0, (uint32_t)(&__bss_end__ - &__bss_start__));
 
@@ -15,8 +15,7 @@ void Reset_Handler(void) {
     for(;;);
 }
 
-__attribute__((section(".isr_vector"), used)) uint32_t vector_table [] = {
+__attribute__((section(".isr_vector"))) volatile uint32_t vector_table [] = {
     (uint32_t)&_MSP,
     (uint32_t)&Reset_Handler,
 };
-
