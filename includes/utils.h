b1cf0d68f2cb427c2a3e48835838fd14c9795302
iiriis <avijitdasxp@gmail.com>
2024-03-03 01:08:14 +0530

    fixed warning

--- includes/utils.h
+++ includes/utils.h
@@ -1,4 +1,4 @@
-#define _chkBit(REG, bit) (REG&(1<<bit)) > 0 ? 1 : 0
+#define _chkBit(REG, bit) ((REG&(1<<bit)) > 0) ? 1 : 0
 #define _setBit(REG, bit) (REG|(1<<bit))
 #define _invBit(REG, bit) (REG^(1<<bit))
 #define _clrBit(REG, bit) (REG&(~(1<<bit)))
